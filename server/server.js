a2a92bff90eaad0c9e9d43e9a140666aa2fe57b3
Srimanta Bose <113753202+BOSE-Srimanta@users.noreply.github.com>
2025-05-13 00:45:35 +0530

    final bosecart app-mern project

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencart-navy.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://bose-cart.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
